b78aaf13b18ccc0a027b56671888c822b798f0c0
Farid <bughunterf@gmail.com>
2026-06-04 13:08:25 +0200

    Comment out unused functions and code in func.go for future reference

--- func.go
+++ func.go
@@ -2,16 +2,34 @@ package main
 
 import "fmt"
 
-func callfunc(callable func(int) int) int {
-	return callable(5)
-}
+// func callfunc(callable func(int) int) int {
+// 	return callable(5)
+// }
 
-func main() {
+// func main() {
 
-	value := callfunc(func(x int) int {
-		return x * x
-	})
+// 	value := callfunc(func(x int) int {
+// 		return x * x
+// 	})
 
-	fmt.Println(value)
+// 	fmt.Println(value)
 	
+// }
+
+// func sum(num ...int) (s int, s2 int) {
+// 	for _, v := range num {
+// 		s += v
+// 		s2 += v * v
+// 	}
+// 	return 
+// }
+
+
+
+func sum(nums ...int) (s int, s2 int) { 
+return
+}
+func main() {
+s, s2 := sum([]int{1, 2, 3, 4, 5}...)
+fmt.Println(s, s2)
 }
