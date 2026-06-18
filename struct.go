28c267f30e80accc23d76be682ba12e447f2f87f
Farid <bughunterf@gmail.com>
2026-06-18 12:17:40 +0200

    Add Sport struct and update person struct to include favorite sports

--- struct.go
+++ struct.go
@@ -2,12 +2,20 @@ package main
 
 import "fmt"
 
+type Sport struct {
+	name string
+	positon string
+}
+
+
 type person struct {
 	name string
 	age  uint
+	favsport []Sport
 }
 
 func main() {
-	p := person{name: "farid", age: 26}
-	fmt.Println(p)
+	p1 := person{ age: 38, name: "Messi", favsport: []Sport{{"Football","Striker"}}}
+	fmt.Println(p1.name, p1.age)
+	fmt.Println(p1.favsport[0].name, p1.favsport[0].positon)
 }
